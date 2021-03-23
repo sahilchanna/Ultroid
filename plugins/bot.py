ff98630af633ad7c4dd63b1a9dedd904f03d7383
sahilchanna <78895649+sahilchanna@users.noreply.github.com>
2021-03-23 12:11:50 +0530

    Update bot.py

--- plugins/bot.py
+++ plugins/bot.py
@@ -99,7 +99,7 @@ async def _(event):
     end = dt.now()
     ms = (end - start).microseconds / 1000
     uptime = grt((time.time() - start_time))
-    await x.edit(get_string("ping").format(ms, uptime))
+    await x.edit(get_string("ping").format("🔥🔥" ms"🔥🔥","🔥🔥"Uptime" 🔥🔥"))
 
 
 @ultroid_cmd(
