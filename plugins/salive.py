738bddaf7627cf5d11c5273e4295ffc83298e1bd
sahilchanna <78895649+sahilchanna@users.noreply.github.com>
2021-03-23 18:55:28 +0530

    Update salive.py

--- plugins/salive.py
+++ plugins/salive.py
@@ -38,7 +38,7 @@ def get_readable_time(seconds: int) -> str:
 uptime = get_readable_time((time.time() - Lastupdate))
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
 PM_IMG = Var.ALIVE_IMAGE
-pm_caption = "➥ **𝔅𝔩𝔞𝔠𝔨 𝔏𝔦𝔤𝔥𝔱𝔫𝔦𝔫𝔤 IS:** `ONLINE`\n\n"
+pm_caption = "➥ **ყσƙσԋαɱα  IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
 pm_caption += "➥ **Python:** `3.7.4` \n"
@@ -48,14 +48,12 @@ pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `{telever}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
 pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](https://github.com/Anmol-dot283/Black-Lightning)\n"
-pm_caption += "➥ **Copyright** : By [KeinShin@Github](GitHub.com/StarkGang)\n"
+pm_caption += "➥ **License** : [GNU General Public License v3.0]()\n"
+pm_caption += "➥ **Copyright** : By [sahilchanna@Github](GitHub.com/sahilchanna)\n"
 pm_caption += "➥ **Check Stats By Doing** `.stat`. \n\n"
-pm_caption += "[🇮🇳 Deploy 𝔅𝔩𝔞𝔠𝔨 𝔏𝔦𝔤𝔥𝔱𝔫𝔦𝔫𝔤Userbot 🇮🇳](https://telegra.ph/FRIDAY-06-15)"
 
 
-@borg.on(lightning_cmd(pattern=r"fralive"))
-@borg.on(sudo_cmd(pattern=r"fralive", allow_sudo=True))
+@ultroid_cmd(pattern=r"fralive"))
 async def friday(alive):
     await alive.get_chat()
     """ For .salive command, check if the bot is running.  """
